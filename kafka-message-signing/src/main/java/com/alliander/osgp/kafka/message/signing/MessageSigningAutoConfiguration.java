2d357c47b73500cb4d5b1f736642a694ceb72fe0
Loes Immens <76959352+loesimmens@users.noreply.github.com>
2024-02-01 16:52:11 +0100

    FDP-1737: optimize imports
    Signed-off-by: Loes Immens <76959352+loesimmens@users.noreply.github.com>

--- kafka-message-signing/src/main/java/com/alliander/osgp/kafka/message/signing/MessageSigningAutoConfiguration.java
+++ kafka-message-signing/src/main/java/com/alliander/osgp/kafka/message/signing/MessageSigningAutoConfiguration.java
@@ -1,16 +1,12 @@
 package com.alliander.osgp.kafka.message.signing;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.util.StreamUtils;
 
 @Configuration
 public class MessageSigningAutoConfiguration {
