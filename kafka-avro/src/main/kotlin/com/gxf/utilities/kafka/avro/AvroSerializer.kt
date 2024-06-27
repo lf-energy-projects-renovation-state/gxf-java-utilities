712354fcaead82c7f94f43645340e92ef6d056a1
Loes Immens <loes.immens@alliander.com>
2024-06-27 15:46:32 +0200

    FDP-2357: one AvroSerializer can now handle all SpecificRecordBase subtypes
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- kafka-avro/src/main/kotlin/com/gxf/utilities/kafka/avro/AvroSerializer.kt
+++ kafka-avro/src/main/kotlin/com/gxf/utilities/kafka/avro/AvroSerializer.kt
@@ -6,15 +6,27 @@ SPDX-License-Identifier: Apache-2.0
 package com.gxf.utilities.kafka.avro
 
 import org.apache.avro.message.BinaryMessageEncoder
-import org.apache.avro.specific.SpecificRecord
+import org.apache.avro.specific.SpecificData
+import org.apache.avro.specific.SpecificRecordBase
 import org.apache.kafka.common.errors.SerializationException
 import org.apache.kafka.common.serialization.Serializer
 import org.slf4j.LoggerFactory
 import java.io.ByteArrayOutputStream
+import kotlin.reflect.KClass
 
-class AvroSerializer<T : SpecificRecord>(private val encoder: BinaryMessageEncoder<T>) : Serializer<T> {
+class AvroSerializer<T : SpecificRecordBase> : Serializer<T> {
+    private val encoders: HashMap<KClass<T>, BinaryMessageEncoder<T>> = HashMap()
+
     companion object {
         private val logger = LoggerFactory.getLogger(AvroSerializer::class.java)
+    }
+
+    private fun getEncoder(message: T): BinaryMessageEncoder<T> {
+        var encoder = encoders[message::class]
+        if (encoder == null) {
+            encoder = BinaryMessageEncoder<T>(SpecificData(), message.schema)
+        }
+        return encoder
     }
 
     /**
@@ -24,6 +36,7 @@ class AvroSerializer<T : SpecificRecord>(private val encoder: BinaryMessageEncod
         try {
             logger.trace("Serializing for {}", topic)
             val outputStream = ByteArrayOutputStream()
+            val encoder = getEncoder(data)
             encoder.encode(data, outputStream)
             return outputStream.toByteArray()
         } catch (ex: Exception) {
