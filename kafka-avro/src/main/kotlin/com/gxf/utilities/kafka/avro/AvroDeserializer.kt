8630446ee99859e69ee335c570fc005bb9d9e163
Jasper Kamerling <jasper.kamerling@alliander.com>
2024-07-16 16:07:51 +0200

    FDP-2470: AvroDeserializer can handle multiple Avro objects
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

--- kafka-avro/src/main/kotlin/com/gxf/utilities/kafka/avro/AvroDeserializer.kt
+++ kafka-avro/src/main/kotlin/com/gxf/utilities/kafka/avro/AvroDeserializer.kt
@@ -5,21 +5,31 @@ SPDX-License-Identifier: Apache-2.0
 */
 package com.gxf.utilities.kafka.avro
 
+import org.apache.avro.Schema
 import org.apache.avro.message.BinaryMessageDecoder
-import org.apache.avro.specific.SpecificRecord
+import org.apache.avro.specific.SpecificData
+import org.apache.avro.specific.SpecificRecordBase
 import org.apache.kafka.common.errors.SerializationException
 import org.apache.kafka.common.serialization.Deserializer
 import org.slf4j.LoggerFactory
 
-class AvroDeserializer<T : SpecificRecord>(private val decoder: BinaryMessageDecoder<T>) : Deserializer<T> {
+class AvroDeserializer(deserializerSchemas: List<Schema>) : Deserializer<SpecificRecordBase> {
     companion object {
         private val logger = LoggerFactory.getLogger(AvroDeserializer::class.java)
     }
 
+    private val decoder = BinaryMessageDecoder<SpecificRecordBase>(SpecificData(), null)
+
+    init {
+        // Add all schema's to the decoder
+        deserializerSchemas
+            .forEach { decoder.addSchema(it) }
+    }
+
     /**
-     * Deserializes a Byte Array to an Avro specific record
+     * Deserializes a Byte Array to an Avro SpecificRecordBase
      */
-    override fun deserialize(topic: String, payload: ByteArray): T? {
+    override fun deserialize(topic: String, payload: ByteArray): SpecificRecordBase {
         try {
             logger.trace("Deserializing for {}", topic)
             return decoder.decode(payload)
