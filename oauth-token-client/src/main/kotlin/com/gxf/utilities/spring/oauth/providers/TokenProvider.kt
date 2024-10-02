0a84dfc6fec241e7c42b5c1b157c23b4251ce9cf
Loes Immens <loes.immens@alliander.com>
2024-10-02 13:52:06 +0200

    FDP-2318: Sonar issue fixed
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- oauth-token-client/src/main/kotlin/com/gxf/utilities/spring/oauth/providers/TokenProvider.kt
+++ oauth-token-client/src/main/kotlin/com/gxf/utilities/spring/oauth/providers/TokenProvider.kt
@@ -1,11 +1,14 @@
+// SPDX-FileCopyrightText: Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
 package com.gxf.utilities.spring.oauth.providers
 
 import java.util.*
 
-interface TokenProvider {
+fun interface TokenProvider {
     /**
-     * Retrieve an access token for instance from an oauth provider.
-     * Tokens are cached internally, there is no need to cache them outside of this library.
+     * Retrieve an access token for instance from an oauth provider. Tokens are cached internally, there is no need to
+     * cache them outside of this library.
      */
     fun getAccessToken(): Optional<String>
 }
