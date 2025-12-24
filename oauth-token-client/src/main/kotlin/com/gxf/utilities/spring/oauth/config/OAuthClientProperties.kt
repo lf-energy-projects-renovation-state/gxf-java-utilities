58c0e693910946bc08a5114b3c4683d161656b7c
Jasper Kamerling <jasper.kamerling@alliander.com>
2025-12-24 12:10:52 +0100

    Remove unused condition
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- oauth-token-client/src/main/kotlin/com/gxf/utilities/spring/oauth/config/OAuthClientProperties.kt
+++ oauth-token-client/src/main/kotlin/com/gxf/utilities/spring/oauth/config/OAuthClientProperties.kt
@@ -3,12 +3,9 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.gxf.utilities.spring.oauth.config
 
-import com.gxf.utilities.spring.oauth.config.condition.OAuthMsalEnabledCondition
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Conditional
 import org.springframework.core.io.Resource
 
-@Conditional(OAuthMsalEnabledCondition::class)
 @ConfigurationProperties(prefix = "oauth.client")
 class OAuthClientProperties(
     val tokenLocation: Resource?,
